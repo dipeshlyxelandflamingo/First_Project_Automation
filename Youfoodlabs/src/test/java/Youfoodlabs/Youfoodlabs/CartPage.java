cc92872bacd92103171978e5f63170c976ca8da7
dipeshlyxelandflamingo <dipesh.singh@lyxelandflamingo.com>
2025-03-07 16:40:44 +0530

    updated testng.xml file

--- Youfoodlabs/src/test/java/Youfoodlabs/Youfoodlabs/CartPage.java
+++ Youfoodlabs/src/test/java/Youfoodlabs/Youfoodlabs/CartPage.java
@@ -1,6 +1,12 @@
 package Youfoodlabs.Youfoodlabs;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.time.Duration;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -8,105 +14,143 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class CartPage {
 
-    WebDriver driver;
+	 WebDriver driver;
+	    FileInputStream file;
+	    FileOutputStream fileOut;
+	    XSSFWorkbook workbook;
+	    XSSFSheet sheet;
+	    DataFormatter formatter;
 
-    @BeforeTest
-    void openBrowser() {
-        // Initialize WebDriver
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get("https://yufoodsco.com/collections/noodle-bowls/products/veg-hakka-noodles-pack-of-3");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    }
+	    @BeforeTest
+	    void openBrowser() throws Exception {
+	        // Initialize WebDriver
+	        WebDriverManager.chromedriver().setup();
+	        driver = new ChromeDriver();
+	        driver.get("https://yufoodsco.com/collections/noodle-bowls/products/veg-hakka-noodles-pack-of-3");
+	        driver.manage().window().maximize();
+	        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-    // Add product into cart and close popup
-    @Test
-    void TC_01() {
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            WebElement Close_popup = wait.until(ExpectedConditions
-                    .elementToBeClickable(By.xpath("//a[@class='js-popup-close newsletter__popup-container-close']")));
-            Close_popup.click();
-            WebElement clickAddToCart = wait.until(
-                    ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='product-add add-pro-cart']")));
-            clickAddToCart.click();
-        } catch (Exception e) {
-            Assert.fail("Error in TC_01: " + e.getMessage());
-        }
-    }
+	        file = new FileInputStream("C:\\Users\\LYXELANDFLAMINGO\\Desktop\\Maintence Project\\Yufoodlabs.xlsx");
+	        workbook = new XSSFWorkbook(file);
+	        sheet = workbook.getSheet("Cart Page");
+	        formatter = new DataFormatter();
+	    }
 
-    // Increase the qty of product on mini cart
-    @Test
-    void TC_02() {
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            WebElement minicart = wait
-                    .until(ExpectedConditions.elementToBeClickable(By.xpath("(//a[normalize-space()='+'])[1]")));
-            minicart.click();
-            Thread.sleep(5000);
-        } catch (Exception e) {
-            Assert.fail("Error in TC_02:" + e.getMessage());
-        }
-    }
+	    // Add product into cart and close popup
+	    @Test
+	    void TC_01() {
+	        try {
+	            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	            WebElement Close_popup = wait.until(ExpectedConditions
+	                    .elementToBeClickable(By.xpath("//a[@class='js-popup-close newsletter__popup-container-close']")));
+	            Close_popup.click();
 
-    // Click On View Cart on Mini Cart to Open Cart Page
-    @Test
-    void TC_03() {
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            WebElement viewCart = wait.until(ExpectedConditions.elementToBeClickable(
-                    By.xpath("//a[@class='ajax-cart__button-view_cart button secondary-button']")));
-            viewCart.click();
-            Thread.sleep(5000);
-        } catch (Exception e) {
-            Assert.fail("Error in TC_03:" + e.getMessage());
-        }
-    }
+	            WebElement clickAddToCart = wait.until(
+	                    ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='product-add add-pro-cart']")));
+	            clickAddToCart.click();
 
-    // Add note
-    @Test
-    void TC_04() {
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            WebElement Addnote = wait
-                    .until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@class='closed'])[1]")));
-            Addnote.click();
-            WebElement update_text = wait.until(ExpectedConditions.elementToBeClickable(By.id("note")));
-            update_text.click();
-            update_text.sendKeys("Pack Food Properly");
-            Thread.sleep(3000);
-        } catch (Exception e) {
-            Assert.fail("Error in TC_04:" + e.getMessage());
-        }
-    }
+	            sheet.getRow(1).createCell(3).setCellValue("Product added to cart successfully");
+	            sheet.getRow(1).createCell(4).setCellValue("PASS");
+	        } catch (Exception e) {
+	            sheet.getRow(1).createCell(3).setCellValue("Failed to add product to cart");
+	            sheet.getRow(1).createCell(4).setCellValue("FAIL");
+	        }
+	    }
 
-    // Click on checkout to redirect checkout page
-    @Test
-    void TC_05() throws Exception {
-        
-            Thread.sleep(3000);
-            JavascriptExecutor js = (JavascriptExecutor) driver;
-            js.executeScript("scroll(0,1000)");
-            Thread.sleep(3000);
-            driver.findElement(By.id("checkout"))
-                    .click();
-            Thread.sleep(3000);
-       
-    }
+	    // Increase the qty of product on mini cart
+	    @Test
+	    void TC_02() {
+	        try {
+	            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	            WebElement minicart = wait
+	                    .until(ExpectedConditions.elementToBeClickable(By.xpath("(//a[normalize-space()='+'])[1]")));
+	            minicart.click();
+	            Thread.sleep(5000);
 
-    @AfterTest
-    void closeBrowser() {
-        // Close the browser
-        driver.quit();
-    }
-}
+	            sheet.getRow(2).createCell(3).setCellValue("Quantity increased successfully");
+	            sheet.getRow(2).createCell(4).setCellValue("PASS");
+	        } catch (Exception e) {
+	            sheet.getRow(2).createCell(3).setCellValue("Failed to increase quantity");
+	            sheet.getRow(2).createCell(4).setCellValue("FAIL");
+	        }
+	    }
+
+	    // Click On View Cart on Mini Cart to Open Cart Page
+	    @Test
+	    void TC_03() {
+	        try {
+	            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	            WebElement viewCart = wait.until(ExpectedConditions.elementToBeClickable(
+	                    By.xpath("//a[@class='ajax-cart__button-view_cart button secondary-button']")));
+	            viewCart.click();
+	            Thread.sleep(5000);
+
+	            sheet.getRow(3).createCell(3).setCellValue("View Cart button clicked successfully");
+	            sheet.getRow(3).createCell(4).setCellValue("PASS");
+	        } catch (Exception e) {
+	            sheet.getRow(3).createCell(3).setCellValue("Failed to click View Cart button");
+	            sheet.getRow(3).createCell(4).setCellValue("FAIL");
+	        }
+	    }
+
+	    // Add note
+	    @Test
+	    void TC_04() {
+	        try {
+	            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	            WebElement Addnote = wait
+	                    .until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@class='closed'])[1]")));
+	            Addnote.click();
+
+	            WebElement update_text = wait.until(ExpectedConditions.elementToBeClickable(By.id("note")));
+	            update_text.click();
+	            update_text.sendKeys("Pack Food Properly");
+	            Thread.sleep(3000);
+
+	            sheet.getRow(4).createCell(3).setCellValue("Note added successfully");
+	            sheet.getRow(4).createCell(4).setCellValue("PASS");
+	        } catch (Exception e) {
+	            sheet.getRow(4).createCell(3).setCellValue("Failed to add note");
+	            sheet.getRow(4).createCell(4).setCellValue("FAIL");
+	        }
+	    }
+
+	    // Click on checkout to redirect checkout page
+	    @Test
+	    void TC_05() {
+	        try {
+	            Thread.sleep(3000);
+	            JavascriptExecutor js = (JavascriptExecutor) driver;
+	            js.executeScript("scroll(0,1000)");
+	            Thread.sleep(3000);
+	            driver.findElement(By.id("checkout")).click();
+	            Thread.sleep(3000);
+
+	            sheet.getRow(5).createCell(3).setCellValue("Redirected to checkout page successfully");
+	            sheet.getRow(5).createCell(4).setCellValue("PASS");
+	        } catch (Exception e) {
+	            sheet.getRow(5).createCell(3).setCellValue("Failed to redirect to checkout page");
+	            sheet.getRow(5).createCell(4).setCellValue("FAIL");
+	        }
+	    }
+
+	    @AfterTest
+	    void closeBrowser() throws Exception {
+	        try {
+	            fileOut = new FileOutputStream("C:\\Users\\LYXELANDFLAMINGO\\Desktop\\Maintence Project\\Yufoodlabs.xlsx");
+	            workbook.write(fileOut);
+	        } finally {
+	            workbook.close();
+	            file.close();
+	        }
+	        driver.quit();
+	    }
+	}
--- Youfoodlabs/src/test/java/Youfoodlabs/Youfoodlabs/Checkout.java
+++ Youfoodlabs/src/test/java/Youfoodlabs/Youfoodlabs/Checkout.java
@@ -1,10 +1,9 @@
 package Youfoodlabs.Youfoodlabs;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.time.Duration;
-import java.util.List;
-import org.testng.asserts.SoftAssert;
-import org.openqa.selenium.interactions.Actions;
+
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -13,129 +12,159 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Checkout {
 
-	WebDriver driver;
-	FileInputStream file;
-	XSSFWorkbook workbook;
-	XSSFSheet sheet;
-	DataFormatter formate;
-	
-	
-	// new changes checkout 
+	 WebDriver driver;
+	    FileInputStream file;
+	    FileOutputStream fileOut;
+	    XSSFWorkbook workbook;
+	    XSSFSheet sheet;
+	    DataFormatter formatter;
 
-	@BeforeTest(description = "openbrowser")
-	void Openbrowser() throws Exception {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.get("https://yufoodsco.com/collections/fruit-juices-100-natural");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	    @BeforeTest(description = "openbrowser")
+	    void Openbrowser() throws Exception {
+	        WebDriverManager.chromedriver().setup();
+	        driver = new ChromeDriver();
+	        driver.get("https://yufoodsco.com/collections/fruit-juices-100-natural");
+	        driver.manage().window().maximize();
+	        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-		file = new FileInputStream("C:\\Users\\LYXELANDFLAMINGO\\Desktop\\Maintence Project\\Yufoodlabs.xlsx");
-		workbook = new XSSFWorkbook(file);
-		sheet = workbook.getSheet("PLP Page");
-		formate = new DataFormatter();
+	        file = new FileInputStream("C:\\Users\\LYXELANDFLAMINGO\\Desktop\\Maintence Project\\Yufoodlabs.xlsx");
+	        workbook = new XSSFWorkbook(file);
+	        sheet = workbook.getSheet("Checkout Page");
+	        formatter = new DataFormatter();
 
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement popup = wait.until(ExpectedConditions.visibilityOfElementLocated(
-				By.xpath("//a[@class='js-popup-close newsletter__popup-container-close']")));
+	        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	        WebElement popup = wait.until(ExpectedConditions.visibilityOfElementLocated(
+	                By.xpath("//a[@class='js-popup-close newsletter__popup-container-close']")));
+	        popup.click();
+	    }
 
-		popup.click();
+	    @Test(description = "Add product in Cart")
+	    void TC_01() {
+	        try {
+	            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	            WebElement addtocart = wait
+	                    .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@id='addToCart'])[2]")));
+	            addtocart.click();
 
+	            sheet.getRow(1).createCell(3).setCellValue("Product added to cart successfully");
+	            sheet.getRow(1).createCell(4).setCellValue("PASS");
+	        } catch (Exception e) {
+	            sheet.getRow(1).createCell(4).setCellValue("FAIL");
+	        }
+	    }
+
+	    @Test(description = "click on checkout button from minicart")
+	    void TC_02() {
+	        try {
+	            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	            WebElement checkout = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("checkout")));
+	            checkout.click();
+
+	            sheet.getRow(2).createCell(3).setCellValue("Checkout button clicked successfully");
+	            sheet.getRow(2).createCell(4).setCellValue("PASS");
+	        } catch (Exception e) {
+	            sheet.getRow(2).createCell(4).setCellValue("FAIL");
+	        }
+	    }
+
+	    @Test(description = "Add contact Email")
+	    void TC_03() {
+	        try {
+	            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	            WebElement addemail = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
+	            addemail.sendKeys("dipesh@yopmail.com");
+
+	            sheet.getRow(3).createCell(3).setCellValue("Email added successfully");
+	            sheet.getRow(3).createCell(4).setCellValue("PASS");
+	        } catch (Exception e) {
+	            sheet.getRow(3).createCell(4).setCellValue("FAIL");
+	        }
+	    }
+
+	    @Test(description = "Add delivery Detail")
+	    void TC_04() {
+	        try {
+	            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	            WebElement fname = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("firstName")));
+	            fname.sendKeys("Dipesh");
+
+	            WebElement lname = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("lastName")));
+	            lname.sendKeys("Sing");
+
+	            Actions ac = new Actions(driver);
+	            WebElement Address = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("shipping-address1")));
+	            Address.sendKeys("supertech");
+	            WebElement Address1 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
+	                    "//span[@aria-label='SUPERTECH ECO VILLAGE-1, Sector 1, Bisrakh Jalalpur, Greater Noida, Uttar Pradesh']")));
+	            Thread.sleep(2000);
+	            ac.moveToElement(Address1).sendKeys(Keys.ENTER).perform();
+
+	            WebElement mobile = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("phone")));
+	            mobile.sendKeys("6354899395");
+
+	            sheet.getRow(4).createCell(3).setCellValue("Delivery details added successfully");
+	            sheet.getRow(4).createCell(4).setCellValue("PASS");
+	        } catch (Exception e) {
+	            sheet.getRow(4).createCell(4).setCellValue("FAIL");
+	        }
+	    }
+
+	    @Test(description = "click on Paynow button")
+	    void TC_05() {
+	        try {
+	            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	            WebElement paynow = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("checkout-pay-button")));
+	            paynow.click();
+
+	            sheet.getRow(5).createCell(3).setCellValue("Pay now button clicked successfully");
+	            sheet.getRow(5).createCell(4).setCellValue("PASS");
+	        } catch (Exception e) {
+	            sheet.getRow(5).createCell(4).setCellValue("FAIL");
+	        }
+	    }
+
+	    @Test(description = "confirm the payment page")
+	    void TC_06() {
+	        SoftAssert softAssert = new SoftAssert();
+	        try {
+	            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	            WebElement iframeElement = wait.until(
+	                    ExpectedConditions.visibilityOfElementLocated(By.xpath("//iframe[@class='razorpay-checkout-frame']")));
+	            driver.switchTo().frame(iframeElement);
+
+	            WebElement paymentPageElement = wait.until(ExpectedConditions
+	                    .visibilityOfElementLocated(By.xpath("//div[@class='flex grow justify-end gap-2 py-2']")));
+	            softAssert.assertTrue(paymentPageElement.isDisplayed(), "Payment page is not visible");
+
+	            sheet.getRow(6).createCell(3).setCellValue("Payment page displayed successfully");
+	            sheet.getRow(6).createCell(4).setCellValue("PASS");
+	        } catch (Exception e) {
+	            sheet.getRow(6).createCell(4).setCellValue("FAIL");
+	        }
+	        softAssert.assertAll();
+	    }
+
+	    @AfterTest
+	    void closebrowser() throws Exception {
+	        try {
+	            fileOut = new FileOutputStream("C:\\Users\\LYXELANDFLAMINGO\\Desktop\\Maintence Project\\Yufoodlabs.xlsx");
+	            workbook.write(fileOut);
+	        } finally {
+	            workbook.close();
+	            file.close();
+	        }
+	        driver.quit();
+	    }
 	}
-
-	@Test(description = "Add product in Cart")
-	void TC_01() {
-
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement addtocart = wait
-				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@id='addToCart'])[2]")));
-		addtocart.click();
-
-	}
-
-	@Test(description = "click on checkout button from minicart")
-	void TC_02() {
-
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement checkout = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("checkout")));
-		checkout.click();
-
-	}
-
-	@Test(description = "Add contact Email")
-	void TC_03() {
-
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement addemail = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
-		addemail.sendKeys("dipesh@yopmail.com");
-
-	}
-
-	@Test(description = "Add delivery Detail")
-	void TC_04() throws Throwable {
-
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-		WebElement fname = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("firstName")));
-		fname.sendKeys("Dipesh");
-
-		WebElement lname = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("lastName")));
-		lname.sendKeys("Sing");
-
-		Actions ac = new Actions(driver);
-		WebElement Address = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("shipping-address1")));
-		Address.sendKeys("supertech");
-		WebElement Address1 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
-				"//span[@aria-label='SUPERTECH ECO VILLAGE-1, Sector 1, Bisrakh Jalalpur, Greater Noida, Uttar Pradesh']")));
-		Thread.sleep(2000);
-		ac.moveToElement(Address1).sendKeys(Keys.ENTER).perform();
-
-		WebElement mobile = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("phone")));
-		mobile.sendKeys("6354899395");
-
-	}
-
-	@Test(description = "click on Paynow button")
-	void TC_05() {
-
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-		WebElement paynow = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("checkout-pay-button")));
-		paynow.click();
-
-	}
-
-	@Test(description = "confirm the payment page")
-	void TC_06() {
-		SoftAssert softAssert = new SoftAssert();
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement iframeElement = wait.until(
-				ExpectedConditions.visibilityOfElementLocated(By.xpath("//iframe[@class='razorpay-checkout-frame']")));
-		driver.switchTo().frame(iframeElement);
-
-		WebElement paymentPageElement = wait.until(ExpectedConditions
-				.visibilityOfElementLocated(By.xpath("//div[@class='flex grow justify-end gap-2 py-2']")));
-		softAssert.assertTrue(paymentPageElement.isDisplayed(), "Payment page is not visible");
-
-		// Mark the test result at the end
-		softAssert.assertAll();
-	}
-
-	@AfterTest
-	void closebrowser() {
-		// Close the browser after the test is complete
-		driver.quit(); // No need for an if condition, it will close the browser if driver is
-						// initialized
-	}
-
-}
